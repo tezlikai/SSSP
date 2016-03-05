93a569e88307048c5e62f0aa84aa886f469cea52
tezlikai <tezlikai@163.com>
2016-03-05 17:30:09 +0800

    添加注解

--- src/com/atguigu/sssp/repository/DepartmentRepository.java
+++ src/com/atguigu/sssp/repository/DepartmentRepository.java
@@ -12,6 +12,11 @@ import com.atguigu.sssp.entity.Department;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer>{
 
+	/**
+	 * 使用二级缓存，需要相应的JAR和对应的配置文件
+	 * 还必须配置@QueryHints({@QueryHint(name=org.hibernate.ejb.QueryHints.HINT_CACHEABLE,value="true")})
+	 * @return
+	 */
 	@QueryHints({@QueryHint(name=org.hibernate.ejb.QueryHints.HINT_CACHEABLE,value="true")})
 	@Query("FROM Department d")
 	List<Department> getAll();
